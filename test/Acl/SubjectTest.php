e5ba6d1af99bf68f7e5f34d2847b86cba99c346d
krisrybak <kris.rybak@hotmail.co.uk>
2020-01-07 13:45:15 +0000

    Adds first test suit

---
+++ test/Acl/SubjectTest.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Elective\SecurityBundle\Tests\Acl;
+
+use PHPUnit\Framework\TestCase;
+
+class SubjectTest extends TestCase
+{
+    public function testTrue()
+    {
+        $this->assertTrue(true);
+    }
+}
