5eceece9b1442757eb329a8b6fdda679db546d32
krisrybak <kris.rybak@hotmail.co.uk>
2020-01-07 15:52:30 +0000

    Adds bundle definition

---
+++ Resources/doc/index.rst
@@ -0,0 +1,11 @@
+The Elective SecurityBundle
+=======================
+
+Installation
+------------
+
+Run this command to install and enable this bundle in your application:
+
+.. code-block:: terminal
+
+    $ composer require electivetechnology/security-bundle
---
+++ src/Acl/ElectiveSecurityBundle.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Elective\SecurityBundle;
+
+use Symfony\Component\HttpKernel\Bundle\Bundle;
+
+/**
+ * Elective\SecurityBundle\ElectiveSecurityBundle
+ *
+ * @author Kris Rybak <kris@elective.io>
+ */
+class ElectiveSecurityBundle extends Bundle
+{
+}
