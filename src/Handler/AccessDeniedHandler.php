78c13b3c06d32f2761130151b5e8d89da88b016f
krisrybak <kris.rybak@hotmail.co.uk>
2020-01-10 13:39:29 +0000

    Adds AccessDeniedHandler

---
+++ src/Handler/AccessDeniedHandler.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace Elective\SecurityBundle\Handler;
+
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
+use Symfony\Component\Security\Http\Authorization\AccessDeniedHandlerInterface;
+
+/**
+ * Elective\SecurityBundle\Handler\AccessDeniedHandler
+ */
+class AccessDeniedHandler implements AccessDeniedHandlerInterface
+{
+    public const ACCESS_DENIED_ERROR = 403180001;
+
+    public function handle(Request $request, AccessDeniedException $accessDeniedException)
+    {
+        $ret = new \StdClass();
+        $ret->message   = 'Access denied';
+        $ret->code      = self::ACCESS_DENIED_ERROR;
+
+        $headers = [
+            'Access-Control-Allow-Origin' => '*'
+        ];
+
+        return new JsonResponse($ret, JsonResponse::HTTP_FORBIDDEN, $headers);
+    }
+}
