5a0d251eb2488edca9e1be1cb7e16fe79f9ed52a
krisrybak <kris.rybak@hotmail.co.uk>
2020-02-04 14:07:23 +0000

    Updates authenticator

---
+++ src/Exception/AuthenticationException.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace Elective\SecurityBundle\Exception;
+
+use Symfony\Component\Security\Core\Exception\AuthenticationException as SecurityAuthenticationException;
+
+/**
+ * Elective\SecurityBundle\Exception\AuthenticationException
+ *
+ * @author Kris Rybak <kris@elective.io>
+ */
+class AuthenticationException extends SecurityAuthenticationException
+{
+}
--- src/Exception/TokenDecoderException.php
+++ src/Exception/TokenDecoderException.php
@@ -9,5 +9,8 @@ namespace Elective\SecurityBundle\Exception;
  */
 class TokenDecoderException extends \Exception
 {
-    public const TOKEN_GENERATOR_ERROR = 500180102;
+    public const TOKEN_DECODER_ERROR            = 500180102;
+    public const TOKEN_DECODER_INVALID_TOKEN    = 401180001;
+    public const TOKEN_DECODER_EXPIRED_TOKEN    = 401180002;
+    public const TOKEN_DECODER_UNVERIFIED_TOKEN = 401180003;
 }
